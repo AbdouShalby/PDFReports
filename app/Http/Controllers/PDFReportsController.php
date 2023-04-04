c590936c036d82775cb1e976e244d6a55684435e
abdoushalby <abdoushalby@gmail.com>
2023-04-04 16:20:09 +0200

    .

--- app/Http/Controllers/PDFReportsController.php
+++ app/Http/Controllers/PDFReportsController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PDFReportsController extends Controller
 {
@@ -10,7 +12,38 @@ class PDFReportsController extends Controller
         return view('reports.create-sick-leave');
     }
     public function storeSickLeave(Request $request) {
-        return view('reports.create-sick-leave');
+        $validator = Validator::make($request->all(), [
+            'leave_start' => 'required|date',
+            'leave_end' => 'required|date',
+            'issue_date' => 'required|date',
+            'name' => 'required|string',
+            'national_id' => 'required|numeric',
+            'nationality' => 'required|string',
+            'employer' => 'required|string',
+            'physician_name' => 'required|string',
+            'position' => 'required|string',
+            'medical_complex' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        function generateLeaveID() {
+            $random = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 6); // generates a random alphanumeric string with a length of 6
+            $date = date('YmdHis'); // gets the current date and time in the format of YYYYMMDDHHIISS
+            return 'S'.$date.$random; // concatenates the string 'L' with the date and the random string
+        }
+
+        $leave = new Leave();
+        $leave->leave_start = $request->input('leave_start');
+        $leave->leave_end = $request->input('leave_end');
+        $leave->leave_id = generateLeaveID();
+        $leave->save();
+
+        return back()->with('success', __('groups.added-success'));
     }
 
     public function createSickLeaveFahd() {
--- resources/views/reports/create-sick-leave.blade.php
+++ resources/views/reports/create-sick-leave.blade.php
@@ -19,50 +19,46 @@
                                 <div class="row">
                                     <div class="col-lg-6">
                                         <div class="form-group">
-                                            <label>{{ __('sick-leave.leave-id') }}</label>
-                                            <input name="leave_id" class="form-control @error('leave_id') is-invalid @enderror" value="{{ old('leave_id') }}" type="text" required placeholder="{{ __('sick-leave.leave-id') }}" autofocus>
-                                        </div>
-                                        <div class="form-group">
                                             <label>{{ __('sick-leave.leave-start') }}</label>
-                                            <input name="leave_start" class="form-control @error('leave_start') is-invalid @enderror" value="{{ old('leave_start') }}" type="date" required placeholder="{{ __('sick-leave.leave-start') }}">
+                                            <input name="leave_start" class="form-control @error('leave_start') is-invalid @enderror" value="{{ old('leave_start') }}" type="date"  placeholder="{{ __('sick-leave.leave-start') }}">
                                         </div>
                                         <div class="form-group">
                                             <label>{{ __('sick-leave.leave-end') }}</label>
-                                            <input name="leave_end" class="form-control @error('leave_end') is-invalid @enderror" value="{{ old('leave_end') }}" type="date" required placeholder="{{ __('sick-leave.leave-end') }}">
+                                            <input name="leave_end" class="form-control @error('leave_end') is-invalid @enderror" value="{{ old('leave_end') }}" type="date"  placeholder="{{ __('sick-leave.leave-end') }}">
                                         </div>
                                         <div class="form-group">
                                             <label>{{ __('sick-leave.issue-date') }}</label>
-                                            <input name="issue_date" class="form-control @error('issue_date') is-invalid @enderror" value="{{ old('issue_date') }}" type="date" required placeholder="{{ __('sick-leave.issue-date') }}">
+                                            <input name="issue_date" class="form-control @error('issue_date') is-invalid @enderror" value="{{ old('issue_date') }}" type="date"  placeholder="{{ __('sick-leave.issue-date') }}">
                                         </div>
                                         <div class="form-group">
                                             <label>{{ __('sick-leave.name') }}</label>
-                                            <input name="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name') }}" type="text" required placeholder="{{ __('sick-leave.name') }}">
+                                            <input name="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name') }}" type="text"  placeholder="{{ __('sick-leave.name') }}">
                                         </div>
                                         <div class="form-group">
                                             <label>{{ __('sick-leave.national-id') }}</label>
-                                            <input name="national_id" class="form-control @error('national_id') is-invalid @enderror" value="{{ old('national_id') }}" type="number" required placeholder="{{ __('sick-leave.national-id') }}">
+                                            <input name="national_id" class="form-control @error('national_id') is-invalid @enderror" value="{{ old('national_id') }}" type="number"  placeholder="{{ __('sick-leave.national-id') }}">
                                         </div>
                                     </div>
                                     <div class="col-lg-6">
                                         <div class="form-group">
                                             <label>{{ __('sick-leave.nationality') }}</label>
-                                            <input name="nationality" class="form-control @error('nationality') is-invalid @enderror" value="{{ old('nationality') }}" type="text" required placeholder="{{ __('sick-leave.nationality') }}">
+                                            <input name="nationality" class="form-control @error('nationality') is-invalid @enderror" value="{{ old('nationality') }}" type="text"  placeholder="{{ __('sick-leave.nationality') }}">
                                         </div>
                                         <div class="form-group">
                                             <label>{{ __('sick-leave.employer') }}</label>
-                                            <input name="employer" class="form-control @error('employer') is-invalid @enderror" value="{{ old('employer') }}" type="text" required placeholder="{{ __('sick-leave.employer') }}">
+                                            <input name="employer" class="form-control @error('employer') is-invalid @enderror" value="{{ old('employer') }}" type="text"  placeholder="{{ __('sick-leave.employer') }}">
                                         </div>
                                         <div class="form-group">
                                             <label>{{ __('sick-leave.physician-name') }}</label>
-                                            <input name="physician_name" class="form-control @error('physician_name') is-invalid @enderror" value="{{ old('physician_name') }}" type="text" required placeholder="{{ __('sick-leave.physician-name') }}">
+                                            <input name="physician_name" class="form-control @error('physician_name') is-invalid @enderror" value="{{ old('physician_name') }}" type="text"  placeholder="{{ __('sick-leave.physician-name') }}">
                                         </div>
                                         <div class="form-group">
                                             <label>{{ __('sick-leave.position') }}</label>
-                                            <input name="position" class="form-control @error('position') is-invalid @enderror" value="{{ old('position') }}" type="text" required placeholder="{{ __('sick-leave.position') }}">
+                                            <input name="position" class="form-control @error('position') is-invalid @enderror" value="{{ old('position') }}" type="text"  placeholder="{{ __('sick-leave.position') }}">
                                         </div>
                                         <div class="form-group">
                                             <label>{{ __('sick-leave.medical-complex') }}</label>
-                                            <input name="medical_complex" class="form-control @error('medical_complex') is-invalid @enderror" value="{{ old('medical_complex') }}" type="text" required placeholder="{{ __('sick-leave.medical-complex') }}">
+                                            <input name="medical_complex" class="form-control @error('medical_complex') is-invalid @enderror" value="{{ old('medical_complex') }}" type="text"  placeholder="{{ __('sick-leave.medical-complex') }}">
                                         </div>
                                     </div>
                                     <div class="col-12 text-center">
